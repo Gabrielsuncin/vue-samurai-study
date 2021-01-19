4e8baee0b286f208ed8ae78b0f7939509267e43b
Gabrielsuncin <gc.everit@itmss.com.br>
2021-01-19 19:07:22 -0300

    updating study vue project

---
+++ vue-teste/src/components/Client.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h2>Domínios</h2>
+    <hr/>
+    <b-container>
+          <div class="form-todo form-group">
+            <p>
+              <input placeholder="Empresa:" class="form-control" type="text" v-model="compannies"/>
+            </p>
+            <p>
+              <input placeholder="Domínio:" class="form-control" v-model="domain"/>
+            </p>
+            <button type="submit" class="btn btn-primary">Adicionar Domínio</button>
+          </div>
+      <hr/>
+      <div class="list-group">
+        <div class="list-group-item" v-for="companny in compannies" :key="companny.id">
+          <span>Enpresa: <strong>{{ companny }}</strong></span>
+          <p>Domínio: {{ domain }}</p>
+          <div>
+            <a href="#" title="Excluir">Excluir</a>
+          </div>
+        </div>
+      </div>
+      <hr/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      compannies: [],
+      domain: [],
+    };
+  },
+};
+</script>
+<style scoped>
+.list-group-item {
+  margin: 12px;
+}
+</style>
---
+++ vue-teste/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Client/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Client from '@/components/Client.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Client
+  }
+}
+</script>
